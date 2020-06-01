55aeb0b6c2b0e589be5c7ef46bfda1417c0960ac
dilrong <wet932@naver.com>
2020-06-01 22:02:37 +0900

    Update:Header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,11 @@ import styled from 'styled-components'
 const Wrap = styled.div`
     background-color: #0F4C81;
     color: white;
-    font-size: 2rem;
-    padding: 1.2rem;
-    text-align: center;
-    margin-bottom: 2rem;
+    font-size: 1.5rem;
+    padding: 1rem;
+    text-align: left;
+    padding-left: 1rem;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
 `
 
 export const Header = ({name}) => {
